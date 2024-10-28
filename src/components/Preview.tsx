726e823c1b53c8b8386688800093a1500ae9377d
albert3318 <bjrexm@gmail.com>
2024-10-27 22:46:28 -0400

    Made the basic code for the timer system on the website, made the arrows be able to loop.

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -8,6 +8,8 @@ const Preview = () => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
     const [data, setData] = useState<any>();
     const [r, setR] = useState<number>(0);
+    const [dontstartTimer, setDST] = useState<number>(0);
+    const [counter, setCounter] = useState<number>(0);
 
     const fetch_data = async () => {
         const response = await fetch("https://store.steampowered.com/appreviews/2821220?json=1&filter=recent&cursor=*");
@@ -16,15 +18,42 @@ const Preview = () => {
         setData(json_data.reviews);
     }
 
-    const update_review_right = () => {if (data && r != data.length-1) setR(r+1);}
-    const update_review_left = () => {if (r != 0) setR(r-1);}
+    const update_review_right = () => {if (data) setR((r+1) % data.length);}
+    const update_review_left_click = () => 
+        {
+            if (r != 0)  {setDST(1); setR((r-1) % data.length); delayForAutoTimer(); } else  { setR(data.length - 1); delayForAutoTimer(); }
+        }
+    const update_review_right_click = () => {setDST(1); if (data) setR((r+1) % data.length); delayForAutoTimer();}
+    
+    const delayForAutoTimer = () => {
+
+        setTimeout(() => {
+
+            setDST(0);
+
+        }, 15000);
+
+    }
 
     useEffect(() =>  {
         if (videoRef.current) videoRef.current.volume = 0.25;
-        
-
         fetch_data();
+
     }, []);
+
+    useEffect(() => {
+        const timer  = setTimeout(() => {
+            if (dontstartTimer == 0)
+            {
+                update_review_right();
+                setCounter(Math.random());
+            }
+        }, 2000);
+        
+        return () => clearTimeout(timer);
+    }, [counter]);
+
+    
 
     return (
         <>
@@ -35,9 +64,9 @@ const Preview = () => {
                 <div className="w-[100vw] h-[100vh] flex-1 overflow-scroll border-b-2 md:border-r-2 md:border-b-0 border-black ">
                     <div className="p-2 h-full">
                         <div className="flex justify-evenly items-center h-8 md:h-16">
-                            {<BsArrowLeftSquareFill onClick={update_review_left} className={`text-black h-full w-auto ${r == 0 ? "invisible" : ""}`}></BsArrowLeftSquareFill>}
+                            {<BsArrowLeftSquareFill onClick={update_review_left_click} className={`text-black h-full w-auto ${r == 0 ? "..." : ""}`}></BsArrowLeftSquareFill>}
                             <p className="text-black text-3xl md:text-6xl text-center font-mono select-none">Reviews</p>
-                            {<BsArrowRightSquareFill onClick={update_review_right} className={`text-black h-full w-auto ${data && r == data.length-1 ? "invisible" : ""}`}></BsArrowRightSquareFill>}
+                            {<BsArrowRightSquareFill onClick={update_review_right_click} className={`text-black h-full w-auto ${data && r == data.length ? "..." : ""}`}></BsArrowRightSquareFill>}
                         </div>
                         <div className="flex flex-col items-center">
                             {   
@@ -49,6 +78,7 @@ const Preview = () => {
                         </div>
                     </div>
                 </div>
+                {/* Literally just the divider between the Video (below) and the card (above) */}
 
                 <div className="w-[100vw] h-[100vh] flex-1 flex justify-center ">
                     <div className="p-2">
